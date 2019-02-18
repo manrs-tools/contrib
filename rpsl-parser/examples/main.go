0256d280e1988d9dbea87f2e0f48e1381f0fc7c3
Chris Morrow <morrowc@ops-netman.net>
2019-02-18 18:46:15 +0000

    Move the example code from the README to an examples/ directory.
    Also correct a few mistakes in the example... which now runs even.

---
+++ rpsl-parser/examples/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	rpsl "github.com/manrs-tools/contrib/rpsl-parser"
+)
+
+const (
+	irrFile = "/tmp/radb.db"
+)
+
+func main() {
+	// Open the irr file, creating an io.Reader for the rpsl.Reader{}.
+	fd, err := os.Open(irrFile)
+	if err != nil {
+		fmt.Printf("Failed to open irrFile(%v): %v\n", irrFile, err)
+		return
+	}
+
+	// Make a new rpsl.Reader{}.
+	rdr := rpsl.NewReader(fd)
+
+	// Read the first character from the file.
+	// Use this character to validate that the file MIGHT be correct.
+	r, _, err := rdr.Read()
+	if err != nil {
+		fmt.Printf("failed to readRune: %v\n", err)
+		return
+	}
+	err = rdr.Unread()
+	if err != nil {
+		fmt.Printf("failed to unRead a rune(%v): %v\n", r, err)
+		return
+	}
+
+	// The file must start with a letter, all IRR records start with a letter character.
+	if !rpsl.IsLetter(r) {
+		fmt.Printf("the initial character read(%v) is not a letter, file unparsable.", r)
+		return
+	}
+
+	// Start to Parse() the reader contents, report results up the channel (rc).
+	rc := make(chan *rpsl.Record)
+	go rpsl.Parse(rdr, rc)
+
+	for rec := range rc {
+		fmt.Printf("Record type(%v):\n", rec.Type)
+		for k, v := range rec.Fields {
+			fmt.Printf("Key(%v)\t-> Val(%v)\n", k, v)
+		}
+		fmt.Println()
+	}
+}
